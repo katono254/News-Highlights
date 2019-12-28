e559f2291e54e98066d481cc4214f9753bb104b6
jakesIII <jayruma@yahoo.com>
2019-12-28 17:40:29 +0300

    Create a function in the view.py in the app

---
+++ app/main/views.py
@@ -0,0 +1,27 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_sources,get_articles
+from ..models import Sources
+
+@main.route('/')
+def index():
+    '''
+    
+    view root page function that returns the index the page and its data 
+    '''
+
+    sources = get_sources()
+    title = "News Highlighter"
+    return render_template('index.html',title = title, sources = sources)
+
+
+@main.route('/sources/<id>')
+def articles(id):
+    '''
+    view articles page
+    '''
+    articles = get_articles(id)
+    title = f'{id}'
+    return render_template('articles.html',title= title,articles = articles)
+
+
