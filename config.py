25013ff819ce087fba4bf8f1aae6cf60df7960e7
jakesIII <jayruma@yahoo.com>
2020-01-06 17:35:29 +0300

    Change indentation on manage.py

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
-class Config 
+class Config:
+ 
   NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
   ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
   NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
   @staticmethod
-    def init_app(app):
+
+  def init_app(app):
         pass
           
 class ProdConfig(Config):
