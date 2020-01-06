191fd4f3918700e2a8c1917e76046512586f8707
jakesIII <jayruma@yahoo.com>
2020-01-06 15:36:34 +0300

    Change the indentation in manage.py

--- manage.py
+++ manage.py
@@ -10,9 +10,10 @@ manager.add_command('server',Server)
 def test():
     """Run the unit tests."""
 import unittest
-      tests = unittest.TestLoader().discover('tests')
-      unittest.TextTestRunner(verbosity=2).run(tests)
+tests = unittest.TestLoader().discover('tests')
+unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 if __name__ == '__main__':
-     manager.run()
+
+    manager.run()
