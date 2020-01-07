1cdfa710b155c080a664b55b24d6ab29579206f0
jakesIII <jayruma@yahoo.com>
2020-01-07 09:44:36 +0300

    Add API key in the app

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 class Config:
  
-  NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
-  ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+  NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?apiKey={8de60e91c78943d2bba5cf8a13cdbe5a}'
+  ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={8de60e91c78943d2bba5cf8a13cdbe5a}'
   NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
   @staticmethod
 
